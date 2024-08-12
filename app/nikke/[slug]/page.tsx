db972442c1c4c3e6a93c319da466f118e9928467
Valentin Marquez <valentin13.mail@gmail.com>
2024-08-12 15:02:49 -0400

    feat: Add page under construction message to nikke/[slug]/page.tsx

--- app/nikke/[slug]/page.tsx
+++ app/nikke/[slug]/page.tsx
@@ -1,1 +1,11 @@
 // TODO: create a webpage to show teams with each nikke page
+
+export default async function Page() {
+  // return page under construction
+
+  return (
+    <div className="flex flex-col w-full">
+      <h1>Page under construction</h1>
+    </div>
+  );
+}
