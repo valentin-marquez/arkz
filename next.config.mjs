2e90f7808005fff2224e0f4bdb8d8174c739593d
Valentin Marquez <valentin13.mail@gmail.com>
2024-08-18 20:27:02 -0400

    chore: Update npm dependencies and add bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -18,4 +20,6 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(nextConfig);
