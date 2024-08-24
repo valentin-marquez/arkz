a60da304798efaef01fcbc4c3b8991ce3dee1021
Valentin Marquez <valentin13.mail@gmail.com>
2024-08-24 04:36:32 -0400

    feat: Add script page for displaying script content

---
+++ app/remote/[name]/page.tsx
@@ -0,0 +1,22 @@
+import fs from "fs";
+import path from "path";
+import { notFound } from "next/navigation";
+
+interface PageProps {
+  params: {
+    name: string;
+  };
+}
+
+export default async function ScriptPage({ params }: PageProps) {
+  const { name } = params;
+  const file = path.join(process.cwd(), "public/scripts", `${name}.ps1`);
+  console.log(file);
+  if (!fs.existsSync(file)) {
+    return notFound();
+  }
+
+  const scriptContent = fs.readFileSync(file, "utf-8");
+
+  return <pre style={{ whiteSpace: "pre-wrap" }}>{scriptContent}</pre>;
+}
